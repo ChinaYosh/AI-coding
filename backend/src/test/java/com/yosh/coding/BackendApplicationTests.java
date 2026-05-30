e94621e172e71b469f0da3376a28fcd30c973c48
ChinaYosh <15549958215@163.com>
2026-05-31 00:04:22 +0800

    初始化后端架构 ---------------------------------common--------------------------------------------------------------------- [BaseRespone]:相当于请求返回，对应之前学过的Result [PageRequest]:分页查询 [ResultUtils]:将BaseRespone封装，构建静态方法 [DeleteRequest]:删除的请求 --------------------------------controller------------------------------------------------------------------ @RestController:是Controller和ResponseBody的组合注解，表示当前类是一个 Spring MVC 控制器，并且该类中方法的返回值会直接写入 HTTP 响应体，通常会被转换成 JSON 返回 @RequestMapping: 是用来建立 请求路径和处理方法之间的映射关系 -----------------------------exception--------------------------------------------------------------------- @Hidden:表示这个类不显示在swagger文档里 @RestControllerAdvice:是ControllerAdive 和 ResponseBody的组合注解 @ControllerAdvice 是 Spring MVC 的全局 Controller 增强注解，常用于统一异常处理、全局数据绑定和全局 Model 数据处理。配合 @ExceptionHandler 可以实现全局异常处理。 @ExceptionHandler:接受一个exception的class文件，对其进行拦截处理。然后Controlle塞到http请求 [ErrorCode]:返回自定义系列的状态码和字符 [BusinessException]:自定义异常，和ErrorCode搭配 [GobalExceptionHander]:全局异常处理，这里拦截自定义Business和runtime异常 [throwUtils]:简化异常处理，只要condition满足就抛相关异常，这里一般是BunessException -------------------------------yml-------------------------
    在server.servlet.context-path中可以通过 /api可以重新规划请求头信息

---
+++ backend/src/test/java/com/yosh/coding/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosh.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
